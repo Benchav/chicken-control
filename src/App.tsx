67c809c6c97cdc5f6cfcc8b190aa013daa27e48f
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-06 08:01:03 -0600

    refactor: add div separator

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ const App = () => (
         <SidebarProvider defaultOpen={true}>
           <div className="min-h-screen flex flex-col md:flex-row w-full">
             <AppSidebar />
+            <div className="flex md:hidden h-[3.5rem]"/>
             <div className="flex-1 flex flex-col">
               <header className="h-14 flex items-center border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4">
                 <SidebarTrigger className="mr-4" />
