294ca03f855fa481e7f3d061caf50f48f8e4016d
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-10-15 21:04:47 -0600

    feat: create user model and add data

---
+++ src/data/user.data.ts
@@ -0,0 +1,16 @@
+import { User } from "@/models/user.model";
+
+export let userData: User[] = [
+  {
+    id: "hjvakca",
+    email: "oscar.molina@gmail.com",
+    password: "123456789",
+    name: "Oscar Danilo Molina",
+  },
+  {
+    id: "hjjhbjkooca",
+    email: "joshua.chavez@gmail.com",
+    password: "123456789",
+    name: "Joshua Chavez"
+  },
+];
---
+++ src/models/user.model.ts
@@ -0,0 +1,7 @@
+export interface User{
+    id?:string;
+    farmId?:string;
+    name:string;
+    email:string;
+    password:string;
+}
